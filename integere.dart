305d8294eb15287af27719f412a9268a2a3b8fed
Rajp459 <98174831+Rajp459@users.noreply.github.com>
2024-07-10 02:47:16 +0530

    Create integere.dart

---
+++ integere.dart
@@ -0,0 +1,11 @@
+void main() {
+//   print("hello raj bhai kaise ho");
+//   var raman = new Human(); //object
+  int a;
+  a=6;
+  print(a);
+}
+
+// class Human {
+//   Human(); //constructor
+// }
