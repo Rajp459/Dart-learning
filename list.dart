883dc911c51204649ad6310356880c5af9b4f51e
Rajp459 <98174831+Rajp459@users.noreply.github.com>
2024-07-11 01:11:19 +0530

    Create list.dart

---
+++ list.dart
@@ -0,0 +1,14 @@
+// List
+//list is similer to an array, which is the ordered collection of objjects
+// var list1 = [10,20,30,40,50];
+void main() {
+  
+  var list = [10,20,30,40];
+  list.add(50);
+  print("$list");
+  var list2 = [];
+  list2.addAll(list);
+  print("$list2");
+  
+ 
+}
