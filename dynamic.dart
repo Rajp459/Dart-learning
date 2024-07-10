b3f42adbc047cf17c73ad58d1924f82d7ee348cc
Rajp459 <98174831+Rajp459@users.noreply.github.com>
2024-07-11 00:39:59 +0530

    Create dynamic.dart

---
+++ dynamic.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("hello raj bhai");
+  String name = "Raj";
+
+  var subject = "math";
+ // subject = 7; it throws error because subject is alredy defined as string
+  var section;//or dynamic section;
+  section = "d";
+  section = 4;
+  section = false;
+}
